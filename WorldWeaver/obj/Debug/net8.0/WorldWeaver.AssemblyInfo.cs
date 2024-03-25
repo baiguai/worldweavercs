86fbe5417e90f18d3a09cee87aa15ee617b3dd08
baiguai <baiguai.junk@gmail.com>
2024-03-24 23:37:27 -0700

    Object Elements
    * Fixed where nested Objects weren't being processed.
    * ToDo: Why is it slower now that cached Elements are being used?

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27f5b8277712b0b177c09f92b463508f615b58f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe28245a39936a3d86bf153d1ef257aada2e64d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
