e1d84cf4b8e7aa15d12049e3142d9064b94cfb17
baiguai <baiguai.junk@gmail.com>
2024-05-28 02:19:46 -0700

    Testing Nested Message Logic
    * Added a nested logic block to the local pub enter message.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37979e65a1beb2b9ee81c9ab1c436862eba78455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16d221fcb4960ee9d73c7fa6edcf965478e2fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
