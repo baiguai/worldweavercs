971df3cd445a1e2d1a121b903b348896a98d1d54
baiguai <baiguai.junk@gmail.com>
2024-06-16 02:47:34 -0700

    Admin Help
    * Defined the Navigation help topic.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4373e36319eb576dfbd9c6f19036e2cde43ce20e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03e528ef352aed158e2779a560a0fd3db6c3e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
