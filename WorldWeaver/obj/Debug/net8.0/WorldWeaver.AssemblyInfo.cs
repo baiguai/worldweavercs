8bedd5921f798145757844216160be1b358350e3
baiguai <baiguai.junk@gmail.com>
2024-06-25 01:13:20 -0700

    Spawning
    * Code and help updates for spawning.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01500b8344f1e9370a3540e966846f3701b1a60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accdd2da09dc0ccc8346f0aa7b1a7121760d5ca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
