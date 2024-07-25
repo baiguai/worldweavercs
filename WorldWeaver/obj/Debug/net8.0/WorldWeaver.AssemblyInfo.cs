c242d6f4d4d84ef4549ac359a5e8657e0ce514d2
baiguai <baiguai.junk@gmail.com>
2024-07-25 02:51:28 -0700

    Test Help
    * Added topics to the Admin Help for tests.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98bfac5cabe9bc1bda50c207315dd37f2a92957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebf79f564c4d33320ddceba4b744c76e17bd60c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
