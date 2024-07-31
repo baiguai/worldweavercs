ed8702ed16ae236faeb025e529a1c88d0083620f
baiguai <baiguai.junk@gmail.com>
2024-07-31 02:51:13 -0700

    Began Main Street
    * Started to define the main street of Kalisto.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9556ef01c28b48b9d9e73f2fc36b0ba63b184ece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68a62ec0574a234fb33b78dc1129409fb6ea414")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
