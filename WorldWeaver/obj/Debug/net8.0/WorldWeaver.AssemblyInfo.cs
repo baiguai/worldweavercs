c3a9c29608885912df50db1a160fe1e91607ba31
baiguai <baiguai.junk@gmail.com>
2024-08-20 00:54:22 -0700

    Armory
    * Updated the DarkWorld definitions for the Armory.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3704fcd40c88155ee43781e97968289c9af1d71a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e48d36f5b48f538f39f057a9e0ea5dd588f97e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
