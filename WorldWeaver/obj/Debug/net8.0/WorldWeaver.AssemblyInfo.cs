c6f9ac5045e1d056a977dedb82b0c7ca4551497e
baiguai <baiguai.junk@gmail.com>
2025-02-08 02:38:49 -0800

    Testing Commit.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeefec306ece72ca8bc3d5c5a8c1c30d9772a214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b783a84c761803d404d92adc0e4d5b9be2999d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
