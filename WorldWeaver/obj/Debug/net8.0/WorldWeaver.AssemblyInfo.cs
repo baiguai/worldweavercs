31c183dc09061ac3f8fc203edb84f3aee6b5e973
baiguai <baiguai.junk@gmail.com>
2025-02-12 02:30:46 -0800

    NPC Follows
    * Created the NPC Follows testing room.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f77aae6797deb9920870d9b75887f0a8bfaebee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ac57ddf86127d77e76faf318e35869aad94780")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
