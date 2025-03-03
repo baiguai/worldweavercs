357375b1bdb1647479fd94816c89b13b490125d2
baiguai <baiguai.junk@gmail.com>
2025-03-03 01:26:14 -0800

    Unit Test
    * Building a unit test.
    * Fixes to arming - when dropping the armed weapon.

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81ae2a0bb29e3307ac88ce9bd8a040b9ad44b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90ca5c22098cd28a796053e77c8ead2599a1dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
