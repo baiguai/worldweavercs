6d2f9656e05f0680ab4d32f65ac90f1c64246184
baiguai <baiguai.junk@gmail.com>
2025-04-12 00:09:08 -0700

    Admin Help Updates

--- WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
+++ WorldWeaver/obj/Debug/net8.0/WorldWeaver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540546d63cedd203203ce6235b0484e35c2072c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d391acc58f90ab0022ae523afb112104b0f07b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldWeaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
